0c237d671632fa57fdb2ac14bdcd8c6a644322ae
Kyrillos Hany <kyrillos.hany@hamza.solutions>
2022-09-13 00:10:58 +0200

    Add Main Heading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,48 @@
-import { projects } from '../utils/projects';
+import { useEffect, useRef } from 'react';
 
 const Home = () => {
-  console.log(projects);
+  const headingRef = useRef(null);
+
+  useEffect(() => {
+    const heading = "Hi,-I'm Kyrillos,-Frontend Developer";
+    const headingLetters = heading.split('');
+
+    headingRef.current.innerHTML = '';
+
+    headingLetters.forEach((letter, ind) => {
+      if (letter === '-') {
+        headingRef.current.innerHTML += '<br>';
+        letter = '';
+      }
+      if (letter === 'K') {
+        headingRef.current.innerHTML += `<span class="letter_one">K</span><span class="letter_two">K</span>`;
+        letter = '';
+      }
+      if (
+        (letter === 'y' && headingLetters[ind + 1] === 'r') ||
+        (letter === 'r' && headingLetters[ind + 1] === 'i') ||
+        (letter === 'i' && headingLetters[ind + 1] === 'l') ||
+        (letter === 'l' && headingLetters[ind + 1] === 'l') ||
+        (letter === 'l' && headingLetters[ind - 1] === 'l') ||
+        (letter === 'o' && headingLetters[ind + 1] === 's') ||
+        (letter === 's' && headingLetters[ind - 1] === 'o') ||
+        (letter === ',' && headingLetters[ind - 1] === 's')
+      ) {
+        headingRef.current.innerHTML += `<span class="others letter">${letter}</span>`;
+        letter = '';
+      }
+
+      const span = document.createElement('span');
+      span.textContent = letter;
+      span.classList.add('letter');
+      headingRef.current.appendChild(span);
+    });
+  }, []);
+
   return (
-    <div>
-      <h2>Home</h2>
-    </div>
+    <section className="home">
+      <h1 className="heading" ref={headingRef}></h1>
+    </section>
   );
 };
 
