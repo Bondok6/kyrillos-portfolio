4a207d865f2f1be287a449063d431199b049068e
Kyrillos Hany <kyrillos.hany@hamza.solutions>
2022-09-13 09:13:35 +0200

    Add Sidebar

--- src/components/Navbar/Sidebar.jsx
+++ src/components/Navbar/Sidebar.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import sidbarLogo from '../../assets/images/nav-logo.PNG';
 
 const Sidebar = () => {
   return (
     <nav className="sidebar">
-      <div className="sidebar__log"></div>
-      <ul className="sidebar__list"></ul>
-      <ul className="sidebar__social"></ul>
+      <div className="sidebar__logo">
+        <img src={sidbarLogo} alt="logo" />
+      </div>
+
+      <ul className="sidebar__list">
+        <li className="sidebar__list-item">
+          <Link to="/about">About</Link>
+        </li>
+        <li className="sidebar__list-item">
+          <Link to="/work">Work</Link>
+        </li>
+        <li className="sidebar__list-item">
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+
+      <ul className="sidebar__social">
+        <li className="sidebar__social-icon">
+          <a href="https://github.com/Bondok6" target="_blank" rel="noreferrer">
+            <i className="fab fa-github-square"></i>
+          </a>
+        </li>
+        <li className="sidebar__social-icon">
+          <a
+            href="https://www.linkedin.com/in/kyrillos-hany/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fab fa-linkedin"></i>
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 };
