9b10d112d13652fe0737a647af613aafed8e189e
Kyrillos Hany <kyrillos.hany@hamza.solutions>
2022-09-15 21:51:15 +0200

    Add mobile menu

--- src/components/Navbar/MenuIcon.jsx
+++ src/components/Navbar/MenuIcon.jsx
@@ -1,24 +1,12 @@
-import React from 'react';
+import OpenMenu from '../../utils/open-menu';
+import CloseMenu from '../../utils/close-menu';
 
-const MenuIcon = () => (
-  <button className="menu-btn">
-    <svg
-      aria-hidden="true"
-      focusable="false"
-      data-prefix="far"
-      data-icon="bars"
-      role="img"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 448 512"
-      class="svg-inline--fa fa-bars fa-w-14 fa-3x"
-    >
-      <path
-        fill="currentColor"
-        d="M436 124H12c-6.627 0-12-5.373-12-12V80c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12zm0 160H12c-6.627 0-12-5.373-12-12v-32c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12zm0 160H12c-6.627 0-12-5.373-12-12v-32c0-6.627 5.373-12 12-12h424c6.627 0 12 5.373 12 12v32c0 6.627-5.373 12-12 12z"
-        class=""
-      ></path>
-    </svg>
-  </button>
-);
+const MenuIcon = ({ isMenuOpen, setIsMenuOpen }) => {
+  return (
+    <button className="menu-btn" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+      {isMenuOpen ? <CloseMenu /> : <OpenMenu />}
+    </button>
+  );
+};
 
 export default MenuIcon;
